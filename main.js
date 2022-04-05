84cc16d51c78cb59eb3674ac8c8428899bd5f01c
Griffin Mara <griffinbob13@gmail.com>
2022-04-05 16:27:01 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var firstName;
+var lastName;
+var age;
+
+firstName = "Griffin";
+lastName = "Mara";
+age = 20
+
+alert(firstName + " " + lastName)
+alert("I am " + age)
+console.log("I am enjoying this class quite a bit, I'm a fan of coding although I'm self taught and not that great")
